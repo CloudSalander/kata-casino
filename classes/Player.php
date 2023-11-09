b509da4331e99fe9df0b3b6e9b13e283360cb023
Cloud <ruben.as532@gmail.com>
2023-11-09 09:51:41 +0100

    Player class completed

--- classes/Player.php
+++ classes/Player.php
@@ -1,7 +1,25 @@
 <?php
 class Player {
 
+	private string $name;
+	private int $profits;
 
+	public function __construct(string $name) {
+		$this->name = $name;
+		$this->profits = 0;
+	}
+
+	public function getName(): string {
+		return $this->name;
+	}
+
+	public function getProfits(): int {
+		$this->profits;
+	}
+
+	public function setProfits(int $profit): void {
+		$this->profits += $profit;
+	}
 
 }
 
