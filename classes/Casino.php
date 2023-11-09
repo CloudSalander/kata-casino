129045e9e8948b92549e3b8011810c42c02976b1
Cloud <ruben.as532@gmail.com>
2023-11-09 10:08:17 +0100

    What I did in 30 minutes

--- classes/Casino.php
+++ classes/Casino.php
@@ -1,6 +1,26 @@
 <?php
 
 class Casino {
-	
-}
+	private array $players;
+
+	public function __construct() {
+		$this->players = [];
+	}
+
+	public function addPlayer(string $player) {
+		var_dump($this->hasPlayer($player));
+		if($this->hasPlayer($player)) {
+			echo $player->getName()." is already in the casino";		
+		}
+		else {
+			$this->players[$player] = 0;
+		}
+	}
+
+	private function hasPlayer(string $player): bool {
+		return array_key_exist($player,$this->players);
+	}
+
+
+} 
 ?>
--- index.php
+++ index.php
@@ -1,3 +1,12 @@
 <?php 
+include_once('classes/Casino.php');
+
+$casino = new Casino();
+
+$casino->addPlayer("Anna");
+$casino->addPlayer("Pep");
+$casino->addPlayer("J.Laporta");
+$casino->addPlayer("Anna"); 
+
 
 ?>
